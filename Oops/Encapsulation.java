14a0cb0245ed4a4fe828255ed9b769b51d484c50
Krrishjaiswal <krrishjais45678@gmail.com>
2026-03-24 18:13:30 +0000

    Encapsulation

---
+++ Oops/Encapsulation.java
@@ -0,0 +1,9 @@
+// Encasulation is a mechanism through which we can wrapping the data members and membere method of class in a single unit
+
+// Note
+// Declare class variables as a private
+// Declare class methods as a public
+
+class a{
+    
+}
--- Oops/Polymorphism.java
+++ Oops/Polymorphism.java
@@ -2,4 +2,57 @@
 
 // two types of polymosrphism
 // compiletime
-// Runtime
+
+class A{
+    int Add()
+    {
+        int a=10; b=20,c;
+        c=a+b;
+        return c;
+    }
+    void add(int x, int y)
+    {
+        int c;
+        c=x+y;
+        System.out.println(c);
+    }
+    void add(int x, double y)
+    {
+        int c;
+        c=x+y;
+        System.out.println(c);
+    }
+    public static void main(String[] args) {
+        A r=new A();
+        r.add(100, 200);
+        r.add(50, 45.32);
+
+        int add = r.add();
+        System.out.println(add);
+    }
+}
+
+// Runtime 
+
+class shape
+{
+    void draw()
+    {
+        System.out.println("Can't say shape type");
+    }
+}
+class square extends shape
+{
+    @Override
+    void draw()
+    {
+        System.out.println("square shape");
+    }
+}
+class Demo
+{
+    public static void main(String[] args) {
+        shape r = new square();
+        r.draw();
+    }
+}
